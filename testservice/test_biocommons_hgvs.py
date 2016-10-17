aa5adcc1614e782d33a2573f8e33d0691429cdad
Justin Paschall <justinpaschalldna@gmail.com>
2016-10-17 19:00:24 -0400

    Changed Biocommons class name and use VariantMapper

--- testservice/test_biocommons_hgvs.py
+++ testservice/test_biocommons_hgvs.py
@@ -1,9 +1,9 @@
-from hgvs_biocommons import Biocommons_hgvs
+from biocommonsService import BiocommonsService
 
 
-my_biocommons_hgvs = Biocommons_hgvs()
+my_biocommons_hgvs = BiocommonsService()
 
-print "t_to_g: " + str(my_biocommons_hgvs.project_t_to_g("NM_001637.3:c.1582G>A"))
+print "t_to_g: " + str(my_biocommons_hgvs.project_t_to_g("NM_001637.3:c.1582G>A", "NC_000007.13"))
 print "g_to_t: " + str(my_biocommons_hgvs.project_g_to_t("NC_000007.13:g.36561662C>T", "NM_001637.3"))
 print "c_to_p: " + str(my_biocommons_hgvs.project_c_to_p("NM_001637.3:c.1582G>A"))
 print "norm: " + str(my_biocommons_hgvs.rewrite("NM_001637.3:c.1582G>A"))
